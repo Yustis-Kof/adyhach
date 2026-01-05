c5458769d3976ac70867767b8f08d70bb1830b7a
Yustis-Kof <kofyustis@yandex.ru>
2026-01-06 00:56:03 +0300

    pagination (yes, before even posting, what will you do to me)

--- imageboard/board/views.py
+++ imageboard/board/views.py
@@ -3,22 +3,31 @@ from django.http import HttpResponse
 from .models import Thread, Post
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import ListView
 
 
 def home(request):
     return render(request, 'home.html')
 
+
 def faq(request):
     return render(request, 'faq.html')
 
-def b(request):
-    threads = Thread.objects.all()
-    print(threads.first().post_set.first().id)
-    #  
-    context = {
-        'name': 'Бред',
-        'threads': threads
-    }
+#def b(request):
+#    threads = Thread.objects.all()
+#    print(threads.first().post_set.first().id)
+#    #  
+#    context = {
+#        'name': 'Бред',
+#        'threads': threads
+#    }
+#
+#
+#    return render(request, 'board.html', context)
 
-
-    return render(request, 'board.html', context)
+class board(ListView):
+    model = Thread
+    template_name = 'board.html'
+    context_object_name = 'threads'
+    ordering = '-id'
+    paginate_by = 2
