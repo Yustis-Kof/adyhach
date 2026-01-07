d4b530c6b2a32816720081083daf2a50eddb3428
Yustis-Kof <kofyustis@yandex.ru>
2026-01-08 00:42:56 +0300

    minor fixes

--- imageboard/board/signals.py
+++ imageboard/board/signals.py
@@ -7,15 +7,15 @@ def set_local_id(sender, instance, **kwargs):
     # Все посты этой доски. Да, я удивлён, что в django так можно. Удобно.
     # last_post = sender.objects.all().filter(thread__board=sender.thread.board).last()
     # Но я всё же лучше реализую, добавив борде атрибут last
-    instance.local_id = instance.thread.board.last + 1
-    
+    if instance.local_id is None:
+        instance.local_id = instance.thread.board.last + 1
+
 
 
 @receiver(post_save, sender=Post)
 def update_last(sender, instance, created, **kwargs):
     if created:
         thread = instance.thread
-
 
         thread.last = instance.local_id
         thread.save()
